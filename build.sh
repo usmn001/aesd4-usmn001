c11e07dcd3894a96f298cc5b55c4d065eef860d1
musman <m_usman010@yahoo.com>
2026-04-26 13:46:35 +0100

    Fixed:build.sh

--- build.sh
+++ build.sh
@@ -21,17 +21,17 @@ then
 	then
 		echo "USING ${AESD_MODIFIED_DEFCONFIG}"
 		echo "USING BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} O=output defconfig BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
+		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
 	else
 		echo "Run ./save_config.s+h to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
 		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
 		echo "USING BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} O=output BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
+		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG}
 	fi
 else
 	echo "USING EXISTING BUILDROOT CONFIG"
 	echo "To force update, delete .config or make changes using make menuconfig and build again."
 	echo "USING BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}"
-	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} O=output BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
+	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} 
 
 fi
